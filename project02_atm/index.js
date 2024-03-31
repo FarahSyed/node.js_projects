c6bbd8d5b79e20016bb305d1b2a9a362bd444d07
FarahSyed <farahsyed44@gmail.com>
2024-03-31 07:35:07 +0500

    add atm

--- project02_atm/index.js
+++ project02_atm/index.js
@@ -1,35 +1,44 @@
+#! /usr/bin/env node
 import inquirer from "inquirer";
-let balance = 200000;
-let pin = 1234;
+// Initial account balance
+let balance = 20000;
+let pin = 1234; // Actual Pin
+// User given pin
 let userPin = await inquirer.prompt({
     name: "pin",
     type: "number",
     message: "Please enter your pin",
 });
+// Check if the user given pin is strictly equals to the actual pin
 if (userPin.pin === pin) {
     console.log("Proceeding to the next step");
+    // Give the user some options
     let operation = await inquirer.prompt({
         name: "action",
         type: "list",
         message: "Please select an operation",
         choices: ["Check Balance", "Withdraw"],
     });
+    // Check user selected operation to perform the operation accordingly
     if (operation.action === "Check Balance") {
         console.log(`Your current balance is ${balance}`);
     }
     else if (operation.action === "Withdraw") {
+        // If user selected withdraw give some more options
         let withdraw = await inquirer.prompt({
             name: "options",
             type: "list",
             message: "Please select an amount",
             choices: ["1000", "2000", "5000", "10000", "Enter Amount"],
         });
+        // Check user selected options to deduct the amount accordingly
         if (withdraw.options === "Enter Amount") {
             let userAmount = await inquirer.prompt({
                 name: "amount",
                 type: "number",
                 message: "Please enter an amount"
             });
+            // If the user enters more amount than their balance OR less than or equal to 0 show message
             if (userAmount.amount > balance || userAmount.amount <= 0) {
                 console.log(`You have insufficient balance`);
             }
