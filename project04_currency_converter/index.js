1ac02da52509e32c4efd0a686cfb5da69ee917d5
FarahSyed <farahsyed44@gmail.com>
2024-04-22 19:19:29 +0500

    add currency converter

---
+++ project04_currency_converter/index.js
@@ -0,0 +1,52 @@
+#! /usr/bin/env node
+import inquirer from 'inquirer';
+import chalk from 'chalk';
+const line = '='.repeat(10);
+const circle = '●';
+console.clear();
+console.log(chalk.cyan.bold(`\n\t ${circle} ${line} "Welcome to Currency Converter" ${line} ${circle}\n`));
+// Exchange rates against the base currency (USD)
+const currencies = {
+    USD: 1, // Base Currency
+    EUR: .94,
+    CHF: .91,
+    GBP: .81,
+    INR: 83.38,
+    PKR: 278.54,
+    JPY: 154.74,
+    KRW: 1380.64,
+    KWD: .31,
+    BHD: .38,
+};
+let userAnswer = await inquirer.prompt([
+    {
+        name: 'amount',
+        message: 'Enter the amount',
+        type: 'input',
+        validate(num) {
+            num = +num;
+            return isNaN(num) ? "Please enter a valid amount" : num <= 0 ? "Please enter a valid amount" : true;
+        },
+    },
+    {
+        name: 'from',
+        message: 'Select  the currency you want to convert from',
+        type: 'list',
+        choices: Object.keys(currencies),
+    },
+    {
+        name: 'to',
+        message: 'Select the currency you want to convert to',
+        type: 'list',
+        choices: Object.keys(currencies),
+    },
+]);
+let amount = Number(userAnswer.amount); // Amount entered by the user
+let from = userAnswer.from; // User's currency
+let to = userAnswer.to; // Currency user wants to convert to
+let fromAmount = currencies[from];
+let toAmount = currencies[to];
+let baseAmount = amount / fromAmount; // Amount converted to USD as base currency
+let convertedAmount = baseAmount * toAmount; // Amount converted to the selected currency
+// Display the converted amounts with proper formatting
+console.log(chalk.cyan.bold(`\n\t${!Number.isInteger(amount) ? amount.toFixed(2) : amount} ${from} = ${!Number.isInteger(convertedAmount) ? convertedAmount.toFixed(2) : convertedAmount} ${to}`));
